206ee5086d80353517833d1b585ff855c77498fc
Michiel <m.a.noback@pl.hanze.nl>
2019-03-21 11:34:27 +0100

    first html of final assign

--- data/intro_lesson_data.R
+++ data/intro_lesson_data.R
@@ -1,13 +1,8 @@
-students <- data.frame(sid=paste0("S0", 1:5), 
-                       name=c("Mark", "Lynn", "Lianne", "Peter", "Rose"),
-                       sex=factor(c("m", "f", "f", "m", "f"), labels = c("female", "male")),
-                       biology=c(5.6, 6.2, 7.9, 4.4, 9.1),
-                       statistics=c(6.1, 5.1, 8.0, 4.7, 7.3),
-                       informatics=c(6.3, 6.1, 7.7, 5.4, 9.5),
+students <- data.frame(sid = paste0("S0", 1:5), 
+                       name = c("Mark", "Lynn", "Lianne", "Peter", "Rose"),
+                       sex = factor(c("m", "f", "f", "m", "f"), labels = c("female", "male")),
+                       biology = c(5.6, 6.2, 7.9, 4.4, 9.1),
+                       statistics = c(6.1, 5.1, 8.0, 4.7, 7.3),
+                       informatics = c(6.3, 6.1, 7.7, 5.4, 9.5),
                        stringsAsFactors = F)
 
-protein_data <- read.table(file = "data/protein_processing_pred.csv",
-                           header = TRUE,
-                           sep = ";",
-                           dec = ",",
-                           as.is = c(1, 2, 3, 21))
